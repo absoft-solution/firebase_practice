4badf2d441126b1e5e96894e0b3432f19835e286
Abdullah Anwar <info.ibnanwar@gmail.com>
2024-07-03 16:17:34 +0500

    Set-up Firebase Connectivity

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebasse_practice/firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
