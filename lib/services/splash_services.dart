e912e5a81682273214f3802da8960c49af25f796
Abdullah Anwar <info.ibnanwar@gmail.com>
2024-07-08 11:43:24 +0500

    add and fetch screen for firestore added

--- lib/services/splash_services.dart
+++ lib/services/splash_services.dart
@@ -5,21 +5,26 @@ import 'package:firebasse_practice/view/home_screen.dart';
 import 'package:firebasse_practice/view/login_screen.dart';
 import 'package:flutter/material.dart';
 
+import '../view/firebase_data_list_screen.dart';
 import '../view/post_screen.dart';
 
 class SplashServices {
-
   void isLogin(BuildContext context) {
     final auth = FirebaseAuth.instance;
     final user = auth.currentUser;
 
     if (user != null) {
-    Timer(Duration(seconds: 5),()=> Navigator.push(context, MaterialPageRoute(builder: (context)=>PostScreen())));
+      Timer(
+          Duration(seconds: 5),
+          () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => FirebaseDataListScreen())));
+    } else {
+      Timer(
+          Duration(seconds: 5),
+          () => Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginScreen())));
     }
-    else
-      {
-        Timer(Duration(seconds: 5),()=> Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen())));
-
-      }
   }
 }
--- lib/view/post_screen.dart
+++ lib/view/post_screen.dart
@@ -22,36 +22,42 @@ class _PostScreenState extends State<PostScreen> {
   Future<void> MyDialog(String title, String id) async {
     _editController.text = title;
     return showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("Update"),
-            content: Container(
-              child: TextFormField(
-                controller: _editController,
-              ),
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Update Post"),
+          content: TextFormField(
+            controller: _editController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Edit Post",
             ),
-            actions: [
-              TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text("Cancel")),
-              TextButton(
-                  onPressed: () {
-                    databaseRef.child(id).update({
-                      'Post': _editController.text.toString(),
-                    }).then((value) {
-                      Navigator.pop(context);
-                    }).catchError((e) {
-                      Fluttertoast.showToast(msg: e.toString());
-                      Navigator.pop(context);
-                    });
-                  },
-                  child: Text("Update")),
-            ],
-          );
-        });
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                databaseRef.child(id).update({
+                  'Post': _editController.text.toString(),
+                }).then((value) {
+                  Navigator.pop(context);
+                  Fluttertoast.showToast(msg: "Post updated successfully");
+                }).catchError((e) {
+                  Fluttertoast.showToast(msg: e.toString());
+                  Navigator.pop(context);
+                });
+              },
+              child: Text("Update"),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -59,24 +65,25 @@ class _PostScreenState extends State<PostScreen> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.blue,
+          backgroundColor: Colors.blueAccent,
           title: Text("Post Screen"),
           centerTitle: true,
           actions: [
             IconButton(
-                onPressed: () {
-                  auth.signOut().then((value) {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()));
-                  }).catchError((error) {
-                    Fluttertoast.showToast(msg: error.toString());
-                  });
-                },
-                icon: Icon(
-                  Icons.logout_outlined,
-                  size: 30,
-                  color: Colors.white,
-                ))
+              onPressed: () {
+                auth.signOut().then((value) {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
+                }).catchError((error) {
+                  Fluttertoast.showToast(msg: error.toString());
+                });
+              },
+              icon: Icon(
+                Icons.logout,
+                size: 30,
+                color: Colors.white,
+              ),
+            ),
           ],
         ),
         floatingActionButton: FloatingActionButton(
@@ -84,84 +91,119 @@ class _PostScreenState extends State<PostScreen> {
             Navigator.push(context,
                 MaterialPageRoute(builder: (context) => AddPostScreen()));
           },
-          child: Icon(
-            Icons.add,
-          ),
+          child: Icon(Icons.add),
         ),
         body: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(10.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Using StreamBuilder"),
-              Expanded(
-                child: Card(
-                    color: Colors.lightGreen.shade100,
-                    child: StreamBuilder(
-                      stream: databaseRef.onValue,
-                      builder:
-                          (context, AsyncSnapshot<DatabaseEvent> snapshot) {
-                        if (!snapshot.hasData) {
-                          return CircularProgressIndicator();
-                        } else {
-                          Map<dynamic, dynamic> map =
-                              snapshot.data!.snapshot.value as dynamic;
-                          List<dynamic> list = [];
-                          list.clear();
-                          list = map.values.toList();
-                          return ListView.builder(
-                              itemCount:
-                                  snapshot.data!.snapshot.children.length,
-                              itemBuilder: (context, index) {
-                                return ListTile(
-                                  title: Text(list[index]['Post'] ?? 'No Data'),
-                                  subtitle:
-                                      Text(list[index]['id'] ?? 'No ID found'),
-                                );
-                              });
-                        }
-                      },
-                    )),
+              Text(
+                "Using StreamBuilder",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
-              Text("Using Firebase Animated List"),
               Expanded(
                 child: Card(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  color: Colors.lightGreen.shade100,
+                  child: StreamBuilder(
+                    stream: databaseRef.onValue,
+                    builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+                      if (!snapshot.hasData) {
+                        return Center(child: CircularProgressIndicator());
+                      } else {
+                        Map<dynamic, dynamic> map =
+                            snapshot.data!.snapshot.value as dynamic;
+                        List<dynamic> list = [];
+                        list.clear();
+                        list = map.values.toList();
+                        return ListView.builder(
+                          itemCount: snapshot.data!.snapshot.children.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.only(right: 6, left: 6),
+                              child: Card(
+                                margin: const EdgeInsets.symmetric(vertical: 5),
+                                child: ListTile(
+                                  title: Text(
+                                    list[index]['Post'] ?? 'No Data',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  subtitle: Text(
+                                    list[index]['id'] ?? 'No ID found',
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ),
+              Text(
+                "Using Firebase Animated List",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Expanded(
+                child: Card(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
                   color: Colors.lightBlue.shade100,
                   child: FirebaseAnimatedList(
-                      query: databaseRef,
-                      itemBuilder: (context, snapshot, animation, index) {
-                        return ListTile(
-                          title: Text(snapshot.child('Post').value.toString()),
-                          subtitle: Text(snapshot.child('id').value.toString()),
-                          trailing: PopupMenuButton(
-                            child: Icon(Icons.more_vert),
-                            itemBuilder: (context) => [
-                              PopupMenuItem(
+                    query: databaseRef,
+                    itemBuilder: (context, snapshot, animation, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 6, left: 6),
+                        child: Card(
+                          margin: const EdgeInsets.symmetric(vertical: 5),
+                          child: ListTile(
+                            title: Text(
+                              snapshot.child('Post').value.toString(),
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text(
+                              snapshot.child('id').value.toString(),
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                            trailing: PopupMenuButton(
+                              icon: Icon(Icons.more_vert),
+                              itemBuilder: (context) => [
+                                PopupMenuItem(
                                   child: ListTile(
-                                leading: Icon(Icons.edit),
-                                title: Text("Edit"),
-                                onTap: () {
-                                  MyDialog(
-                                      snapshot.child('Post').value.toString(),
-                                      snapshot.key.toString());
-                                },
-                              )),
-                              PopupMenuItem(
+                                    leading: Icon(Icons.edit),
+                                    title: Text("Edit"),
+                                    onTap: () {
+                                      MyDialog(
+                                        snapshot.child('Post').value.toString(),
+                                        snapshot.key.toString(),
+                                      );
+                                    },
+                                  ),
+                                ),
+                                PopupMenuItem(
                                   child: ListTile(
-                                leading: Icon(Icons.delete),
-                                title: Text("Delete"),
-                                onTap: () {
-                                  databaseRef
-                                      .child(snapshot.key.toString())
-                                      .remove();
-                                  Navigator.pop(context);
-                                },
-                              )),
-                            ],
+                                    leading: Icon(Icons.delete),
+                                    title: Text("Delete"),
+                                    onTap: () {
+                                      databaseRef
+                                          .child(snapshot.key.toString())
+                                          .remove();
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        );
-                      }),
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              )
+              ),
             ],
           ),
         ),
